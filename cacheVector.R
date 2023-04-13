d26a8cb5f8d10fbf06788eadadebc4eac0354d6c
Tandin Dorji <tndn.dorji@gmail.com>
2023-04-13 21:53:02 +1000

    committing makeVector and cacheMean functions re-written to practice

--- cacheVector.R
+++ cacheVector.R
@@ -1,15 +1,37 @@
-makeVector <- function(x = numeric()){
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeVector <- function(x = numeric()) {
     m <- NULL
     set <- function(y) {
-            x <<- y
-            m <<- NULL
+        x <<- y
+        m <<- NULL
     }
     get <- function() x
     setmean <- function(mean) m <<- mean
     getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+    list(
+        set = set,
+        get = get,
+        setmean = setmean,
+        getmean = getmean
+    )
 }
 
-cacheMean <- 
+
+## Write a short comment describing this function
+
+cacheMean <- function(x, ...) {
+    ## Return the mean of 'x'
+    m <- x$getmean()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
