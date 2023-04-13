8df2dd2f91e3d22efdae7af0558413a8dfb41575
Tandin Dorji <tndn.dorji@gmail.com>
2023-04-13 21:52:28 +1000

    first commit of final function cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions returns the inverse of a matrix. 
+## If the inverse is there in the global environment, the value is returned.
+## Otherwise, inverse of the matrix is calculated and returned.
 
-## Write a short comment describing this function
+## this function creates a special vector which contains function to:
+## set the value of matrix
+## get the value of matrix
+## set the value of inverse
+## get the value of inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(
+        set = set,
+        get = get,
+        setinv = setinv,
+        getinv = getinv
+    )
 }
 
 
-## Write a short comment describing this function
+## this function gets the inverse of the matrix from cache, if present
+## and returns it. Otherwise, it computes the inverse and returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
